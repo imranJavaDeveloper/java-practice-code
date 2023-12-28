e2ca69e144b89d68049899b69746fc8afe0ecdee
Md Imran Alam <mdimranalam2348@gmail.com>
2023-12-28 13:28:35 +0530

    Calculate simple interest

---
+++ 01_Basic/Simple_interest.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+public class Simple_interest {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        // Scanner is used for taking input from the user
+
+        // calculate simple interest
+        System.out.println("enter the principal balance ");
+        float balance = sc.nextFloat();
+
+        System.out.println(" enter the rate of interest");
+        float interest=sc.nextFloat();
+
+        System.out.println(" enter the time");
+        float time= sc.nextFloat();
+
+        float SI=(balance*interest*time)/100;
+
+        System.out.println(" total interest ="+SI);
+    }
+}
