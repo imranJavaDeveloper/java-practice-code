925dfde820dbe2060bb825045e81006e6ef2e556
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-08 15:34:57 +0530

    Create Skip_Sequence.java

---
+++ Basic/Skip_Sequence.java
@@ -0,0 +1,33 @@
+package Basic;
+
+public class Skip_Sequence {
+    public static void main(String[] args) {
+
+
+        System.out.println("\n");
+        System.out.println("\t");
+        System.out.println("\r");
+        System.out.println("\b");
+        System.out.println("\f");
+
+        System.out.println("\"");
+        System.out.println("\"");
+        System.out.println("\\");
+        System.out.println("\'");
+        System.out.println("\"");
+
+        System.out.println("\"hello\"");                                   // "hello"
+
+        System.out.println(" \"hello my name is md imran alam\"");         // "md imran alam"
+
+        System.out.println( "md imran\' alam");                            // md imran' alam
+
+        System.out.println("\\ md imran alam \\");                         //  \md imran alam\
+
+
+        // four ways to various char literal
+
+        char ch=0Xface;
+        System.out.println(ch);
+   }
+}
