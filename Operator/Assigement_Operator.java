2b9445feffab592de0bc438e182ba8cd11e31c0e
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-08 16:47:32 +0530

    Create Assigement_Operator.java

---
+++ Operator/Assigement_Operator.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class Assigement_Operator {
+    public static void main(String[] args) {
+
+        // Assignment operators
+        int f = 7;
+        System.out.println("f += 3: " + (f += 3));
+        System.out.println("f -= 2: " + (f -= 2));
+        System.out.println("f *= 4: " + (f *= 4));
+        System.out.println("f /= 3: " + (f /= 3));
+        System.out.println("f %= 2: " + (f %= 2));
+        System.out.println("f <<= 2: " + (f <<= 2));
+        System.out.println("f >>= 1: " + (f >>= 1));
+        System.out.println("f >>>= 1: " + (f >>>= 1));
+
+    }
+}
