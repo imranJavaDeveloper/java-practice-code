fc1d6982b20789e4a9274096fa770a6c73e071b9
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-13 01:32:16 +0530

    Some Tricky Question

---
+++ Conditional_Statement/Example_04.java
@@ -0,0 +1,22 @@
+public class Example_04 {
+    
+    // some trickliy Question
+    public static void main(String[] args) {
+        
+        //if(a<=0)
+{
+  // if(a==0)
+   {
+     System.out.println("1 ");
+   }
+   //else 
+   { 
+      System.out.println("2 ");
+   }
+}
+System.out.println("3 ");
+    }
+}
+
+
+// Answer will be three 3 
