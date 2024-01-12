1e90099759bf4bfb01cc740a1125974666efaf89
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-12 23:45:31 +0530

    check in two variable which value is big

---
+++ Conditional_Statement/Example2.java
@@ -0,0 +1,13 @@
+public class Example2 {
+    
+        public static void main(String[] args) {
+               
+            // check in two variable wchich value is big
+                    int x = 20;
+                    int y = 18;
+                    if (x > y) {
+                        System.out.println("x is greater than y");
+                    }
+    
+        }
+    }
