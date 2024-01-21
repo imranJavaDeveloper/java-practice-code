3068844da9126982e9128f8323514947160835fe
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-21 22:25:54 +0530

    Adding Roll Using the Setter And Getter Method in java

--- setter_and_getter/GteSet.java
+++ setter_and_getter/GteSet.java
@@ -4,6 +4,8 @@ public class GteSet {
     // class level Variable
     private String name;
 
+    private int roll;
+
     //  method 1 getter
     public String getName()
     {
@@ -12,6 +14,12 @@ public class GteSet {
     //  method 2 setter
     public void setName(String name){
           this.name=name;
+    }
+    public void setRoll(int roll){
+        this.roll=roll;
+    }
+    public int getroll(){
+        return roll;
     }
 }
 
@@ -24,10 +32,13 @@ class GFG extends GteSet {
     {
         // Creating an object of class 1 in main() method
         GteSet obj = new GteSet();
+           
  
         // Setting the name by calling setter method
         obj.setName("Geeks for Geeks");
+        obj.setRoll(101);
         // Getting the name by calling getter method
         System.out.println(obj.getName());
+        System.out.println(obj.getroll());
     }
 }
