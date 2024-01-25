50fd6d22d6cb084bed5920d83d10feb64d7e7130
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-25 23:58:00 +0530

    Create ArrayList_03.java

---
+++ 10_Collection_FrameWork/ArrayList_03.java
@@ -0,0 +1,35 @@
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList_03 {
+    public static void main(String[] args) {
+
+        // create ArrayList
+
+        ArrayList<String> list= new ArrayList<>();
+              list.add("Md Imran alam");
+              list.add("Md Asad alam");
+              list.add("Md Raja alam");
+
+        // Remove data from the ArrayList
+        list.remove("Md Imran Alam");
+        System.out.println(list);
+
+        // Add element based on index
+        list.add(1,"md Adil Alam");
+        System.out.println(list);
+
+        // Set value for a element
+        list.set(2,"Md Rahat Ali");
+
+        // for print all value
+
+        Iterator<String> it= list.iterator();
+
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+}
