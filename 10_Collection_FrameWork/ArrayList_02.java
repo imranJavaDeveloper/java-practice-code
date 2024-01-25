7a9ea341fe00661ee323cad3411b3d93b953576c
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-26 00:22:12 +0530

    Remove A element Using the iterator

---
+++ 10_Collection_FrameWork/ArrayList_02.java
@@ -0,0 +1,30 @@
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class ArrayList_02 {
+    public static void main(String[] args) {
+
+        ArrayList l= new ArrayList();
+
+        // Add element
+
+        l.add(1);
+        l.add(2);
+        l.add(3);
+        l.add(4);
+        l.add(5);
+
+        // remove a element form the ArrayList using iterator
+
+        Iterator<Integer> it= l.iterator();
+        while (it.hasNext()){
+            int item= it.next();
+            if(item%2==0){
+                it.remove();
+            }
+            System.out.println(l);
+        }
+    }
+}
