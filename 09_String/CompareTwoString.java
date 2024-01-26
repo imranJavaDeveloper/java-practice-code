62a3908a92fd71d8787f03cbc2041660d42b4cbc
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-26 16:29:54 +0530

    Create CompareTwoString.java

---
+++ 09_String/CompareTwoString.java
@@ -0,0 +1,14 @@
+package 09_String;
+
+public class CompareTwoString {
+    public static void main(String[] args) {
+        
+        String str="core java";
+        String str2="core java";
+        System.out.println(str==str2);// true
+
+        String name1=new String("platfrom indepedent");
+        String name2=new String("platfrom indepedent");
+    }
+    
+}
