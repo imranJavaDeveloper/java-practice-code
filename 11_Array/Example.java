ca5f8cf38a4b32c6c7abf8fe85b974643decf710
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-27 01:05:44 +0530

    Create Example.java

---
+++ 11_Array/Example.java
@@ -0,0 +1,23 @@
+
+public class Example {
+    
+    public static void main(String[] args) {
+        
+        // creating int array
+
+        int [] marks={98,58,78,65,78};
+
+        // print array using the array index
+          System.out.println(marks[0]);
+          System.out.println(marks[1]);
+          System.out.println(marks[2]);
+          System.out.println(marks[3]);
+          System.out.println(marks[4]);
+
+          // Print array using for each loop
+
+          for(int x :marks){
+            System.out.println(x);
+          }
+    }
+}
