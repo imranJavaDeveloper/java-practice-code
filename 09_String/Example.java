2b184c2bc327cba310acd84c9d9bf55cb5fafebd
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-26 16:24:26 +0530

    Create Example.java

---
+++ 09_String/Example.java
@@ -0,0 +1,20 @@
+public class Example{
+    public static void main(String[] args) {
+        
+        // Creating String like Literal
+
+        String str= "Md Furkan Alam";
+        String str2="Md Imran Alam";
+
+        System.out.println(str);// md furkan alam
+        System.out.println(str2);// md imran alam
+
+         // Creating the String using new keyword
+
+         
+        String name= new String("Md Adil Alam");
+        String name2=new String("Md Asif Alam");
+
+
+        }
+}
