f6a4f5d4177eb7924e3f44e962f1c576c2ec5ee6
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-29 22:25:41 +0530

    Create Exeception_handlin1.java

---
+++ 11_Exeption_Handling/Exeception_handlin1.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+public class Exeception_handlin1 {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println(" enter two number");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+
+        int c=a/b;
+        System.out.println("division is="+c);
+
+        int d=a+b;
+        System.out.println(" Addition of a+b="+d);
+
+
+    }
+}
