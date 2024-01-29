0b525aba96a57044cc31ba8df537fdd5185c16ed
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-29 22:19:48 +0530

    Create FirstSevenNumber.java

---
+++ 02_Conditional_Statement/FirstSevenNumber.java
@@ -0,0 +1,15 @@
+
+
+public class FirstSevenNumber {
+    public static void main(String[] args) {
+
+        // sum of first seven natural number
+
+        int sum=0;
+        for (int i=0;i<=7;i++){
+            sum=sum+i;
+
+        }
+        System.out.println("sum of first seven number "+sum);
+    }
+}
