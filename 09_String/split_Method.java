6d7293c23c10039239b7e0a184e004e15cbe71f9
Md Imran Alam <mdimranalam2348@gmail.com>
2024-01-31 00:43:54 +0530

    Create split_Method.java

---
+++ 09_String/split_Method.java
@@ -0,0 +1,22 @@
+
+public class split_Method {
+    public static void main(String[] args) {
+
+        // split method using
+
+//        String name="this is imran";
+//
+//        String [] str=name.split(" \\s ");
+//
+//        for(String x: str){
+//            System.out.print(x);
+
+
+        String name="md furkan alam";
+
+        String []str=name.split(" \\s");
+        for (String  x:str){
+            System.out.println(x);
+        }
+    }
+}
