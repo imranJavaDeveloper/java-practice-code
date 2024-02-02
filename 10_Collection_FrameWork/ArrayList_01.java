4b42ccbf81c28b3e671216bd275131a713a6da66
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-02 22:43:56 +0530

    Update ArrayList_01.java

--- 10_Collection_FrameWork/ArrayList_01.java
+++ 10_Collection_FrameWork/ArrayList_01.java
@@ -8,6 +8,8 @@ public class ArrayList_01 {
         // Create ArrayList and show all element
 
         ArrayList<Integer> list= new ArrayList<>(10);
+        // create String ArrayList
+        ArrayList<String> str=new ArrayList<>();
         // Add element
            list.add(10);
            list.add(20);
@@ -16,7 +18,16 @@ public class ArrayList_01 {
            list.add(50);
            list.add(60);
            list.add(70);
-           // show all data
+
+           //Add String type value 
+           str.add("mran");
+           str.add("Raja");
+           str.add("Atif");
+           str.add("Adil");
+           str.add("Shahid");
+           // show all String type data
+           System.out.println(str);
+           // show all Integer type data
         System.out.println(list);
 
     }
