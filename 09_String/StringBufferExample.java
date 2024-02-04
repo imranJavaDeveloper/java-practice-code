41eed9b854db39bf58c447dbc786239b1f9a5f3e
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-04 23:49:47 +0530

    Create StringBufferExample.java

---
+++ 09_String/StringBufferExample.java
@@ -0,0 +1,10 @@
+public class StringBufferExample {
+    public static void main(String[] args) {
+        
+        StringBuffer sb= new StringBuffer("furkan");
+        StringBuffer sb2 = new StringBuffer("imran");
+
+            System.out.println(sb);
+            System.out.println(sb2);
+    }
+}
