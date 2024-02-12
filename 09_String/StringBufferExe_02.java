5543b96ce6ebeec6826e6536edfd0b196f517312
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-12 23:41:19 +0530

    Create StringBufferExe_02.java

---
+++ 09_String/StringBufferExe_02.java
@@ -0,0 +1,11 @@
+public class StringBufferExe_02 {
+    
+    public static void main(String[] args) {
+        
+        StringBuffer sb= new StringBuffer("hello");
+        // append() use for adding two String 
+           sb.append("world");
+
+           System.out.println(sb);
+    }
+}
