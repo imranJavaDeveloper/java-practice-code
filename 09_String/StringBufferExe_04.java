0f792bf31399816a88430d498f4a0d1e18b39af5
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-12 23:50:04 +0530

    Create StringBufferExe_04.java

---
+++ 09_String/StringBufferExe_04.java
@@ -0,0 +1,11 @@
+public class StringBufferExe_04 {
+   public static void main(String[] args) {
+    
+    // reverse()
+    StringBuffer sb= new StringBuffer("java programming");
+         sb.reverse();
+    // After reverse the string      
+          System.out.println(sb);
+        
+   } 
+}
