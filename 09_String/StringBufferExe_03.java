e5296a2993fc5cec8410cbdc063c1653faf17acb
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-12 23:44:16 +0530

    Create StringBufferExe_03.java

---
+++ 09_String/StringBufferExe_03.java
@@ -0,0 +1,10 @@
+public class StringBufferExe_03 {
+
+    public static void main(String[] args) {
+        
+        // insert method()
+        StringBuffer sb= new StringBuffer("hello");
+           sb.insert(1, "java");
+           System.out.println(sb);
+    }
+}
