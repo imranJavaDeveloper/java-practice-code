0c969a14fba4e08657bdb97d56491e2552bed4c5
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-16 00:52:13 +0530

    Create Fuctional_Interface_03.java

---
+++ 15_lambda_Feature/Fuctional_Interface_03.java
@@ -0,0 +1,11 @@
+
+public interface Fuctional_Interface_03 {
+    public void m1(int a,int b);
+}
+class Test3{
+    public static void main(String[] args) {
+
+        Fuctional_Interface_03 obj= (int a,int b)->{System.out.println(a+b);};
+        obj.m1(10,50); // output will come 60
+    }
+}
