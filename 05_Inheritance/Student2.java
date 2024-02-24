fffe9d95c3ff7e149b44b7c4ac462b2d3962ae7a
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-24 16:42:55 +0530

    Create Student2.java

---
+++ 05_Inheritance/Student2.java
@@ -0,0 +1,46 @@
+
+public class Student2 {
+    String name;
+    int roll;
+
+    // constructor
+    Student2(String name,int roll){
+        this.name=name;
+        this.roll=roll;
+    }
+}
+
+class Teacher extends Student2{
+
+    double salary;
+
+   Teacher(String name, int roll,double salary) {
+        super(name,roll);
+
+        this.name=name;
+        this.salary=salary;
+        this.roll=roll;
+    }
+
+    Teacher(String name, int roll) {
+        super(name, roll);
+    }
+
+    // create show method
+    public void show(){
+        System.out.println("Name :"+name);
+        System.out.println("salary :"+salary);
+        System.out.println("Roll :"+roll);
+    }
+
+    public static void main(String[] args) {
+
+        Teacher obj= new Teacher("md imran alam",101,45000);
+        obj.show();
+        System.out.println("================================");
+        Teacher obj2= new Teacher("md Raza alam alam",102,45800);
+        obj.show();
+
+    }
+
+}
