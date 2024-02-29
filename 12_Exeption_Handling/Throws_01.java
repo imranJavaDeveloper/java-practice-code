14b1e86d20b10c0b6a3d4735e5eb374ee52c4124
Md Imran Alam <mdimranalam2348@gmail.com>
2024-02-29 23:04:56 +0530

    Update Throws_01.java

--- 12_Exeption_Handling/Throws_01.java
+++ 12_Exeption_Handling/Throws_01.java
@@ -4,7 +4,7 @@ import java.io.PrintWriter;
 
 public class Throws_01 {
     public static void main(String[] args) throws FileNotFoundException {
-
+        // Throws keyword used to declare the execption
         PrintWriter pw= new PrintWriter("hello");
         System.out.println("hello");
     }
