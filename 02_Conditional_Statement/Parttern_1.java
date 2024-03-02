af53874e62f9d9f605ee33bf92e836c3ed67dd52
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-02 23:36:39 +0530

    Parttern Example

--- 02_Conditional_Statement/Parttern_1.java
+++ 02_Conditional_Statement/Parttern_1.java
@@ -7,7 +7,7 @@ public static void main(String []args){
      * ******
      */
 
-     for(int i=0;i<6;i++){
+     for(int i=0;i<4;i++){
         for(int j=0;j<6;j++){
 
         System.out.print("*");
