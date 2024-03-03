2955caaf12688cef82df16e4188cbf4e47e286b7
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-03 23:59:01 +0530

    Update PrimeNumber.java

--- 02_Conditional_Statement/PrimeNumber.java
+++ 02_Conditional_Statement/PrimeNumber.java
@@ -7,13 +7,19 @@ Scanner sc = new Scanner(System.in);
 
         System.out.println("enter a number ");
         int number =sc.nextInt();
-        
-          for(int i=2;i<number;i++){
+        int i;
+          for( i=2;i<=number-1;i++){
 
             if (number/i==0) {
                  break;
             }
-          }   
+          } 
+          if(i==number){
+            System.out.println("number is prime :"+number);
+          } 
+          else{
+            System.out.println("Number is not Prime :"+number);
+          } 
             
     }
 }
