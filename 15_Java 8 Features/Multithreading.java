ced7280dfcda25dadd064efa78ca74abf3c90551
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-04 19:59:55 +0530

    lamda example

---
+++ 15_Java 8 Features/Multithreading.java
@@ -0,0 +1,25 @@
+public class Multithreading  implements Runnable{
+    public void run(){
+        for(int i=0;i<5;i++){
+            System.out.println("run method called");
+        }
+    }
+    
+}
+
+/**
+ * InnerMultithreading
+ */
+ class main {
+    public static void main(String[] args) {
+        
+        Multithreading obj= new Multithreading();
+        Thread t= new Thread(obj);
+
+        t.start();
+
+        for(int i=0;i<5;i++){
+            System.out.println("main method called");
+        }
+    }
+}
