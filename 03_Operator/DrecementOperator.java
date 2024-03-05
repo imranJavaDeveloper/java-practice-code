7a2c398663e4fb9c73d2a8d2048aeb22a67f7df6
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-05 23:52:16 +0530

    increment and Dercement operator example

---
+++ 03_Operator/DrecementOperator.java
@@ -0,0 +1,13 @@
+
+public class DrecementOperator {
+    public static void main(String[] args) {
+
+        int x=55;
+
+        // pre increment
+        System.out.println(--x);
+
+        // post increment
+        System.out.println(x--);
+    }
+}
---
+++ 03_Operator/IncrementOperator.java
@@ -0,0 +1,14 @@
+
+public class IncrementOperator {
+
+    public static void main(String[] args) {
+
+        int x=10;
+
+        // pre increment
+        System.out.println(++x);
+
+        // post increment
+        System.out.println(x++);
+    }
+}
