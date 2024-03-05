9f7d14f93491c9f13a84a0080e90278285aadd65
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-06 00:29:54 +0530

    Create LogicalOperator.java

---
+++ 03_Operator/LogicalOperator.java
@@ -0,0 +1,16 @@
+public class LogicalOperator {
+    public static void main(String[] args) {
+        
+    // Logical Operator
+          int a=78, b=25, c=56;
+
+    // Logical(And &&)  
+       System.out.println((a>b) && (a>c));
+       System.out.println((b>a) && (b>c));
+       System.out.println((c>a) && (c>b));
+       System.out.println((a>b) && (b<c));
+
+       // Logical (or ||)
+    }
+    
+}
