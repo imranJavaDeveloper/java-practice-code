09f936b07ada9b6c1dce4694e28076b3630903c9
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-06 00:04:15 +0530

    Create RelationOperator.java

---
+++ 03_Operator/RelationOperator.java
@@ -0,0 +1,14 @@
+
+public class RelationOperator {
+    public static void main(String[] args) {
+
+        int x=45, b=25;
+
+        System.out.println(x>b);//true
+        System.out.println(x<b); // false
+        System.out.println(x>=b);//ture
+        System.out.println(x<=b);//false
+        System.out.println(x==b); //false
+        System.out.println(x!=b); //ture
+    }
+}
