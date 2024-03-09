9daa2c44a680462b3c78e99894e41018840e3190
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-09 23:38:53 +0530

    Delete Constant_in_java.java

--- 01_Basic/Constant_in_java.java
+++
@@ -1,8 +0,0 @@
-
-
-public class Constant_in_java {
-    public static void main(String[] args) {
-        final String company_name="md imran alam";
-        System.out.println(company_name);
-    }
-}
