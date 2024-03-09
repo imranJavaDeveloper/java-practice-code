6826a9ff10ea5552a57a71993b0055c0129acaff
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-09 23:55:00 +0530

    Update VariableType.java

--- 01_Basic/VariableType.java
+++ 01_Basic/VariableType.java
@@ -1,6 +1,30 @@
 public class VariableType {
+
+    // class level variable
+       String name;
+
+       public void setName(String name){
+        this.name=name;
+       }
+
+       public String getName(){
+         return getName();
+       }
+    // static variable
+
+      public static int a=10;
     public static void main(String[] args) {
         
         // types of variable in java
+         
+        // Local variable
+            
+        int num=78;
+
+        VariableType obj= new VariableType();
+           // Instance variable call After the create object of a class
+             obj.setName("md imran alam");
+            System.out.println( obj.getName());
+        
     }
 }
