28cec909cb2df1627d65f23565faecd6cb15163a
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-12 23:52:56 +0530

    Create MapExe1.java

---
+++ StreamExample/MapExe1.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MapExe1 {
+    public static void main(String[] args) {
+
+        ArrayList<String> vehicles= new ArrayList<>();
+        vehicles.add("car");
+        vehicles.add("bus");
+        vehicles.add("bike");
+        vehicles.add("range rover");
+        vehicles.add("suv");
+
+
+        System.out.println(" all string in lower case");
+        System.out.println(vehicles);
+        // convert all string in upper case
+
+        List<String> vehicles2=vehicles.stream().map(m->m.toUpperCase()).collect(Collectors.toList());
+
+        // print all string upper case latter
+        System.out.println("all string in upper case ");
+        System.out.println(vehicles2);
+        System.out.println("thank you");
+    }
+}
