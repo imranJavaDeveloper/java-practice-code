3a236feaa152d94f2a48c6dd0ec84e4186a8edc0
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-13 23:36:17 +0530

    Update disitintMethod.java

--- StreamExample/disitintMethod.java
+++ StreamExample/disitintMethod.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-
 public class disitintMethod {
     public static void main(String[] args) {
 
