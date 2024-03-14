07a3c3b1c1765bc4a4f1413fa451da22f11160b4
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-14 23:36:42 +0530

    Update FlatMapDemo.java

--- StreamExample/FlatMapDemo.java
+++ StreamExample/FlatMapDemo.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 
 public class FlatMapDemo {
     public static void main(String[] args) {
-
+        // first ArrayList
         ArrayList<Integer> list= new ArrayList<>();
            list.add(78);
            list.add(25);
-
+         // Second ArrayList
         ArrayList<Integer> list1= new ArrayList<>();
         list1.add(55);
         list1.add(78);
@@ -19,6 +19,7 @@ public class FlatMapDemo {
         System.out.println(list2); // [[78, 25], [55, 78]]
 
         // use flat map
+        // print all ArrayList in single list
         List<Integer> list3=list2.stream().flatMap(x->x.stream().map(n->n+10)).collect(Collectors.toList());
         System.out.println(list3);//[78, 25, 55, 78]  add 10 in all element [88, 35, 65, 88]
 
