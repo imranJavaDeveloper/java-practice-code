2b545adea86c8c57bdf9265e0ecc096d3bb0d929
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-17 23:47:10 +0530

    Create Predicate_Exampple_04.java

---
+++ 15_Java 8 Features/Predicate_Exampple_04.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class Predicate_Exampple_04 {
+    public static void main(String[] args) {
+
+        List<Integer> list= Arrays.asList(78,458,58,669,5885,295,588,258,4525,78569,85598,558,8);
+
+        Predicate<Integer> num=(k)->k%2==0;
+        Predicate<Integer> num2=(j)->j>400;
+
+        //traverse ArrayList
+          for (Integer list2 :list){
+              if (num.and(num2).test(list2)){
+                  System.out.println(list2);
+              }
+          }
+    }
+}
