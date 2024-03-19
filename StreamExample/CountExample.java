637c48118a5e3a7e1c87928bd3f3d4e79eff1176
Md Imran Alam <mdimranalam2348@gmail.com>
2024-03-19 23:55:40 +0530

    Update CountExample.java

--- StreamExample/CountExample.java
+++ StreamExample/CountExample.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 public class CountExample {
     public static void main(String[] args) {
 
-
         ArrayList<Integer> list= new ArrayList<>();
         list.add(78);
         list.add(89);
@@ -13,6 +12,7 @@ public class CountExample {
         list.add(78);
         list.add(42);
 
+        // check hom many object present in the list using count()
           long count= list.stream().filter(m->m>50).count();
           System.out.println("print how many number upper 50 :="+count);
     }
