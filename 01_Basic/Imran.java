de7437872fc4fc93fe1cc43a2ebb779ad9f609b8
Md Imran Alam <mdimranalam2348@gmail.com>
2024-04-08 23:12:05 +0530

    Update Imran.java

--- 01_Basic/Imran.java
+++ 01_Basic/Imran.java
@@ -1,5 +1,6 @@
 public class Imran {
     public static void main(String[] args) {
+      
         System.out.println(" hi ");
         System.out.println(" my name is md imran alam");
       System.out.println("i am from bihar ");
