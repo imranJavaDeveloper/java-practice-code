cd9042141229259d4ae45a1afbcc50fb71f9250e
Md Imran Alam <mdimranalam2348@gmail.com>
2024-04-29 14:22:36 +0530

    Create Simple.java

---
+++ 01_Basic/Simple.java
@@ -0,0 +1,10 @@
+public class Simple {
+    public static void main(String[] args) {
+        
+        System.out.println("intorduction");
+        System.out.println("my name is md imran alam");
+        System.out.println("i am from bihar");
+        System.out.println("i have completed B.tech in 2023");
+        System.out.println("thank you");
+    }
+}
