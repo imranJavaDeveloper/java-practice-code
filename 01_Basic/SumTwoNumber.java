1587277fe8361a9852e7008f7e79e65d6c0ab247
Md Imran Alam <mdimranalam2348@gmail.com>
2024-04-29 14:48:31 +0530

    Update SumTwoNumber.java

--- 01_Basic/SumTwoNumber.java
+++ 01_Basic/SumTwoNumber.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
 public class SumTwoNumber {
     public static void main(String[] args) {
         
         // sum of two number 
-          int a=78;
-          int b=24;
-          int c=a+b;
-          System.out.println(" sum of two number"+c);
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter first number ");
+
+        int firstNumber=sc.nextInt();
+
+
+        System.out.println("enter second number ");
+        int secondNumber =sc.nextInt();
+
+        int third=firstNumber+secondNumber;
+
+        System.out.println("the sum of two number : "+third);
     }
 }
