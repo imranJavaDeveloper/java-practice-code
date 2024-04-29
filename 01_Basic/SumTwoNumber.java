c8aa49f84778b9517d15cfb9730416770502bbe9
Md Imran Alam <mdimranalam2348@gmail.com>
2024-04-29 14:39:37 +0530

    Create SumTwoNumber.java

---
+++ 01_Basic/SumTwoNumber.java
@@ -0,0 +1,10 @@
+public class SumTwoNumber {
+    public static void main(String[] args) {
+        
+        // sum of two number 
+          int a=78;
+          int b=24;
+          int c=a+b;
+          System.out.println(" sum of two number"+c);
+    }
+}
