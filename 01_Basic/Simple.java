531374eb18208f779df9818a9b9de9b2719b6eaf
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-01 23:35:40 +0530

    Update Simple.java

--- 01_Basic/Simple.java
+++ 01_Basic/Simple.java
@@ -1,6 +1,6 @@
 public class Simple {
     public static void main(String[] args) {
-        
+
         System.out.println("intorduction");
         System.out.println("my name is md imran alam");
         System.out.println("i am from bihar");
