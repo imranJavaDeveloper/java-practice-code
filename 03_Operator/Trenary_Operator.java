b281d98f7fa0ebc591e1d951672b25b742391f71
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-02 14:11:00 +0530

    Create Trenary_Operator.java

---
+++ 03_Operator/Trenary_Operator.java
@@ -0,0 +1,13 @@
+package operator;
+
+public class Trenary_Operator {
+    public static void main(String[] args) {
+
+        int a = 20, b = 10, c = 30, result;
+
+        // result holds max of three
+        // numbers
+        result = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+        System.out.println("Max of three numbers = "+ result);
+    }
+}
