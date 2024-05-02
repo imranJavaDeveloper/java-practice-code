d6c79dbeda78e32d5858c2e933ef447627fd3e9a
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-02 14:23:02 +0530

    Ternary Operator Exaplme

--- 03_Operator/Trenary_Operator.java
+++ 03_Operator/Trenary_Operator.java
@@ -3,11 +3,11 @@ package operator;
 public class Trenary_Operator {
     public static void main(String[] args) {
 
-        int a = 20, b = 10, c = 30, result;
+        int a = 20, b = 10, c = 30, Max;
 
         // result holds max of three
         // numbers
-        result = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
-        System.out.println("Max of three numbers = "+ result);
+        Max = ((a > b) ? (a > c) ? a : c : (b > c) ? b : c);
+        System.out.println("Max of three numbers = "+ Max);
     }
 }
