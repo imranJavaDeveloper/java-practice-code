9fd38c31af61f4d5a634f664980e21e1145c18b2
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-03 23:25:42 +0530

    Update IncrementOperator.java

--- 03_Operator/IncrementOperator.java
+++ 03_Operator/IncrementOperator.java
@@ -7,9 +7,10 @@ public class IncrementOperator {
 
         // pre increment
         System.out.println(++x);
-        
+
 
         // post increment
         System.out.println(x++);
+        
     }
 }
