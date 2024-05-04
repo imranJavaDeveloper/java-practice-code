6e08afc670187ff9fbdad390bb10edb6b2c4b8ea
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-04 10:57:56 +0530

    Update LogicalOperator2.java

--- 03_Operator/LogicalOperator2.java
+++ 03_Operator/LogicalOperator2.java
@@ -5,18 +5,21 @@ class LogicalOperator2{
 
          int a=78,b=25,c=36;
 
+         // Example of &&(And) Operator
          System.out.println((a>b) && (a>c));//true
          System.out.println((a>b) && (a<c));//false
          System.out.println((b>a) && (b>c));//false
          System.out.println((c>a) && (c>b));//false
  
- 
+         // Example of ||(Or) Operator
+
          System.out.println((a>b) ||(a>c));//true
          System.out.println((a>b) || (a<c));//true
          System.out.println((b>a) || (b>c));//false
          System.out.println((c>a) || (c>b));//true
  
- 
+         // Example of !(Not) Operator
+
          System.out.println(!(a>b)); //false
          System.out.println(!(a<b)); // false
          System.out.println(!(c<a));// false
