c20a083f4cf6c10ffe65270ec869cf0d9d386575
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-04 10:53:53 +0530

    Create LogicalOperator2.java

---
+++ 03_Operator/LogicalOperator2.java
@@ -0,0 +1,27 @@
+class LogicalOperator2{
+    public static void main(String args[]){
+
+         // logical operator example
+
+         int a=78,b=25,c=36;
+
+         System.out.println((a>b) && (a>c));//true
+         System.out.println((a>b) && (a<c));//false
+         System.out.println((b>a) && (b>c));//false
+         System.out.println((c>a) && (c>b));//false
+ 
+ 
+         System.out.println((a>b) ||(a>c));//true
+         System.out.println((a>b) || (a<c));//true
+         System.out.println((b>a) || (b>c));//false
+         System.out.println((c>a) || (c>b));//true
+ 
+ 
+         System.out.println(!(a>b)); //false
+         System.out.println(!(a<b)); // false
+         System.out.println(!(c<a));// false
+         System.out.println(!(c>a));// ture
+
+
+    }
+}
