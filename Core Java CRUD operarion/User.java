9936dd1ed7db6af81c99cf95d6f6d4ac9bfe518e
Md Imran Alam <mdimranalam2348@gmail.com>
2024-05-26 23:59:00 +0530

    Create User.java

---
+++ Core Java CRUD operarion/User.java
@@ -0,0 +1,47 @@
+package Student_Detail;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+
+    public User(int id, String name, String email) {
+    }
+
+    public User() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
