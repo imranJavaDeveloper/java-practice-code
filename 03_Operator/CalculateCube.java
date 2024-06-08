bc73cc8b83283a04db92c3dbba1922de601a5dad
Md Imran Alam <mdimranalam2348@gmail.com>
2024-06-09 00:11:19 +0530

    calculate cube progarm

---
+++ 03_Operator/CalculateCube.java
@@ -0,0 +1,11 @@
+
+public class CalculateCube {
+    public static void main(String[] args) {
+
+
+    // calculate cube 
+        int base = 3, exponent = 2;
+        double result = Math.pow(base, exponent);
+        System.out.println("Cube  = " + result);
+    }
+}
