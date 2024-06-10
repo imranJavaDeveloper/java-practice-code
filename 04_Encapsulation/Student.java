522cc5f993beacc41b303146804a0b1c0c565e0a
Md Imran Alam <mdimranalam2348@gmail.com>
2024-06-10 23:32:21 +0530

    Add Encapsulation Program

--- 04_Encapsulation/Student.java
+++ 04_Encapsulation/Student.java
@@ -1,33 +1,51 @@
-package Encapsulation;
 
-public class Student {
-    private String name;
-    private int roll;
+public class student {
 
-    private int age;
+    int roll;
+    String name;
+    String address;
 
-    // set method for initialize the value 
-    public void setStudent(String n, int r, int a) {
-        name = n;
-        age = a;
-        roll = r;
+    public void setAddress(String address) {
+        this.address = address;
     }
-     
-    // show method for display all the data 
-    public void show() {
-        System.out.println("Name :" + name);
-        System.out.println("Roll :" + roll);
-        System.out.println("age :" + age);
-    }
-}
-        /* create a class for creating the object  */
-    class Main {
-        public static void main(String[] args) {
 
-            Student obj = new Student();
-            obj.setStudent("md imran alam", 101, 26);
-            obj.show();
-
+    public void setRoll(int roll) {
+        this.roll = roll;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void show(){
+        System.out.println(" Roll ="+roll);
+        System.out.println(" Name ="+name);
+        System.out.println("Address ="+address);
+    }
+    // create show method
 }
 
+class UseStudent{
+    public static void main(String[] args) {
+
+        student obj= new student();
+        System.out.println(" first student");
+        obj.setAddress("Gondapur Nawada");
+        obj.setName("Md imran alam");
+        obj.setRoll(101);
+        obj.show();
+
+        System.out.println("===========================");
+        student obj2= new student();
+        System.out.println("second student");
+        obj2.setAddress("Gondapur Nawada");
+        obj2.setName("Md furkan alam alam");
+        obj2.setRoll(102);
+        obj2.show();
+
+
+
+
+
+    }
+}
