45f99d8dc22b69d67d52d54eb7b60eece4b94102
Md Imran Alam <mdimranalam2348@gmail.com>
2024-08-02 00:21:19 +0530

    Update Example.java

--- 15_Java 8 Features/Example.java
+++ 15_Java 8 Features/Example.java
@@ -1,18 +1,21 @@
 
-public class Example {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class Example {
     public static void main(String[] args) {
 
-        Runnable r= new Runnable() {
-            @Override
-            public void run() {
+        List<Integer> list= Arrays.asList(45,21,5,66,59,999,6);
+        List<Integer> list1=Arrays.asList(78,254,5669,48,6698,58);
 
-            }
-        };
+        // use flat map
 
-        Thread t= new Thread(r);
-        t.start();
+        List<List<Integer>> list3=new ArrayList<>();
+            list3.add(list1);
+            list3.add(list);
 
-        System.out.println("main method called");
+            List<Integer> list2= list3.stream().flatMap(x->x.stream().filter(y->y%2==0).collect(Collectors.toList());
     }
 }
