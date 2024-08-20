c8c3c5970f7001011a4f9408410505daa2197fd3
Md Imran Alam <mdimranalam2348@gmail.com>
2024-08-20 23:12:14 +0530

    Create Pattern6.java

---
+++ Pattern_Example/Pattern6.java
@@ -0,0 +1,17 @@
+package Pattern_Example;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+
+        int n=10;
+        int k=1;
+
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                System.out.printf("%2d ",k);
+                k++;
+            }
+            System.out.println();
+        }
+    }
+}
