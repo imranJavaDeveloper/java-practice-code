75b21a4002e8a47733ac77e562ce102d0e691ee8
imranJavaDeveloper <151560129+imranJavaDeveloper@users.noreply.github.com>
2025-04-03 17:51:44 +0530

    Update MyClass.java

--- 01_Basic/MyClass.java
+++ 01_Basic/MyClass.java
@@ -2,6 +2,20 @@ public class MyClass {
 
     public static void main(String[] args) {
         
-        /*** */
+        /** Example of literal */
+
+        /*** Interger literal  how many ways we can define  */
+        //  decimal form
+
+        int x=10;
+
+        // octal form
+        
+        int x2=010;
+
+        // hexadecimal form
+        int x3=0x10;
+
+        System.out.println(x+" "+x2+" "+" "+x3); // 10 8 16
     }
 }
