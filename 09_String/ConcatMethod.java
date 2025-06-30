1fdc9e447d40342b5bafbb94e3659bfd072ed740
Md Imran Alam <mdimranalam2348@gmail.com>
2025-06-30 23:25:11 +0530

    Create ConcatMethod.java

---
+++ 09_String/ConcatMethod.java
@@ -0,0 +1,15 @@
+
+public class ConcatMethod {
+
+    public static void main(String[] args) {
+
+        // add two or more String
+
+        String s1= new String("furkan");
+        String s2= new String("imran");
+
+        System.out.println(s1+" "+s2); // furkan imran
+
+        System.out.println(s1.concat(s2)); // furkanimran
+    }
+}
