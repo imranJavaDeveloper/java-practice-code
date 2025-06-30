13e3e1fba3a1dce86931b30b50b9e2a9b09182a7
Md Imran Alam <mdimranalam2348@gmail.com>
2025-06-30 23:20:52 +0530

    Create CharAtMethod.java

---
+++ 09_String/CharAtMethod.java
@@ -0,0 +1,19 @@
+
+public class CharAtMethod {
+    public static void main(String[] args) {
+
+        String s="apple";
+
+        // example of charAt
+        System.out.println(s.charAt(2));// p
+
+        String name= new String("furkan");
+
+        System.out.println(name.charAt(0));// f
+
+
+        String str=" Apple is Good  ";
+        System.out.println(str.charAt(2));//p
+
+    }
+}
