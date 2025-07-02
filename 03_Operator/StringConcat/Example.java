9ffe416970a3405a4b69f5217fc79d654825bcc2
Md Imran Alam <mdimranalam2348@gmail.com>
2025-07-02 21:23:45 +0530

    Create Example.java

---
+++ 03_Operator/StringConcat/Example.java
@@ -0,0 +1,11 @@
+
+public class Example {
+    public static void main(String[] args) {
+
+        System.out.println(10+20);//30
+        System.out.println("hi"+"md");//himd
+
+        System.out.println("abc"+1+2); //abc12
+        System.out.println(1+2+"abc");//3abc
+    }
+}
