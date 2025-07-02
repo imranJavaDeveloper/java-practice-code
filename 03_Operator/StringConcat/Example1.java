6325185e32ba9625e2d502f42e49b391624d73c3
Md Imran Alam <mdimranalam2348@gmail.com>
2025-07-02 21:22:43 +0530

    Create Example1.java

---
+++ 03_Operator/StringConcat/Example1.java
@@ -0,0 +1,17 @@
+
+
+public class Example1 {
+
+    public static void main(String[] args) {
+
+        // String concat Example between int or String
+
+          String s="durga";
+          int a=10,b=20,c=30;
+
+        System.out.println(s+a+b+c);
+        System.out.println(a+b+c);
+        System.out.println(b+c+a+s);
+        System.out.println(c+a+s+b);
+    }
+}
