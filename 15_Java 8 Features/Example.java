d7e1bb19fe94fad7bd7416106de1afb086aeed0f
Md Imran Alam <151560129+imranJavaDeveloper@users.noreply.github.com>
2025-07-26 23:09:54 +0530

    Add files via upload

--- 15_Java 8 Features/Example.java
+++ 15_Java 8 Features/Example.java
@@ -1,21 +1,43 @@
+package Collection;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+
+
+class MyComparator implements Comparator<Integer>{
+
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        if (o1<o2){
+            return -1;
+        } else if (o1>o2) {
+            return +1;
+        }
+        else {
+            return 0;
+        }
+
+    }
+}
 
 public class Example {
     public static void main(String[] args) {
 
-        List<Integer> list= Arrays.asList(45,21,5,66,59,999,6);
-        List<Integer> list1=Arrays.asList(78,254,5669,48,6698,58);
+        ArrayList<Integer> list= new ArrayList<>();
+        list.add(45);
+        list.add(450);
+        list.add(505);
+        list.add(650);
+        list.add(15);
+        list.add(65);
 
-        // use flat map
 
-        List<List<Integer>> list3=new ArrayList<>();
-            list3.add(list1);
-            list3.add(list);
+        System.out.println(list);
+        ///  natural sorting
+        Collections.sort(list);
 
-            List<Integer> list2= list3.stream().flatMap(x->x.stream().filter(y->y%2==0).collect(Collectors.toList());
+        System.out.println(list);
     }
 }
