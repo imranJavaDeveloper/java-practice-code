c397b253300b605af1369aeb41437f3b89b17fb7
Md Imran Alam <mdimranalam2348@gmail.com>
2025-08-23 16:08:06 +0530

    example of preincrement operator

---
+++ 03_Operator/Increment And Decrement Operator/PreIncrementOperatorExample.java
@@ -0,0 +1,27 @@
+
+
+public class PreIncrementOperatorExample {
+
+    public static void main(String[] args) {
+
+        int x=45;
+        int x2=++x;
+
+        ///  first increment than return updated value
+
+        System.out.println(x);//46
+        System.out.println(x2);//46
+
+
+         char ch='A';
+         char ch2=++ch;
+        System.out.println(ch);//B
+        System.out.println(ch2);//B
+
+           double d=45024.54;
+           double d2=++d;
+
+        System.out.println(d);//45025.54
+        System.out.println(d2);//45025.54
+    }
+}
